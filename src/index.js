ba7df14f6f2017bee947c980a2c334bceafeeee5
masha1705 <mmmaria.mail@mail.ru>
2022-03-18 15:41:18 +0500

    'Build props interactions'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+        className="square"
+      >
+        {this.props.value}
       </button>
     );
   }
@@ -13,7 +16,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
