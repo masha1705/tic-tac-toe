3a341aaf677a768f6d8f557079104609188e412d
masha1705 <mmmaria.mail@mail.ru>
2022-03-21 13:33:49 +0500

    Add winner

--- src/index.js
+++ src/index.js
@@ -45,7 +45,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.nextStep ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status = '';
+
+    if (winner) {
+      status = `The winner is ${calculateWinner(this.state.squares)}`
+
+    } else {
+      status = `Next player: ${this.state.nextStep ? 'X' : 'O'}`;
+    }
 
     return (
       <div>
@@ -84,6 +92,28 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 ReactDOM.render(
